f79c38566adc11ac8e8b6f856ed8ed055b6a7eaf
Alex <alex.pappa.1@gmail.com>
2022-04-07 10:55:22 -0400

    update

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 module.exports={
-    secret:"a2e536a92152456e81d3f1ae3cbb68d6d4e84eb4192a4cvb5ada7789823jklhasdf0sddfskjlhfdhjp9o234yhtr89opdfshgn0p89wq34hgnesaoldfslkjahsdfjhkhguibnbhnbnhm525hf52nr8gdfs9gs9fgsadfjklhtrui945",
-    database:"mongodb+srv://fizzingIdiot:YXpJFGTqZfKWfZz5@cluster0.u7n18.mongodb.net/Cluster0?retryWrites=true&w=majority",
+    secret:process.env.JWT_SECRET,
+    database: process.env.DATABASE_CONN,
     maxAge:"1d"
 }
