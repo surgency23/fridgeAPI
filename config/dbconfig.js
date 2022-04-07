9b3e380cbf2845a5f944d73bcbd63a69e52ff250
Alex <alex.pappa.1@gmail.com>
2022-04-07 11:14:03 -0400

    keys update

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 module.exports={
     secret:process.env.JWT_SECRET,
-    database: process.env.DATABASE_CONN,
+    database: `mongodb+srv://${DATABASE_USER}:${DATABASE_PASS}@${DATABASE_NAME}.u7n18.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`,
     maxAge:"1d"
 }
