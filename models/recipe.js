9085c36954f56f1d16f285ce6ca911045a5ce885
Alex Pappa <alex.pappa.1@gmail.com>
2022-04-19 10:10:33 -0400

    update

--- models/recipe.js
+++ models/recipe.js
@@ -2,9 +2,35 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeScheme = new Schema({
+    tags:{
+        type:Array,
+        require:true
+    },
     servings:{
         type:String,
+        require:false
+    },
+    recipeSource:{
+        type:String,
+        require:false
+    },
+    imageSource:{
+        type:String,
+        require:false
+    },
+    image:{},
+    instructions:{
+        type:String,
         require:true
-    }
+    },
+    ingredients:{
+        type:Array,
+        require:true
+    },
+    title:{
+        type:String,
+        require:true
+    },
+    parsedIngredients:{type:Array,require:false}
 });
 module.exports = mongoose.model('Recipe',recipeScheme);
